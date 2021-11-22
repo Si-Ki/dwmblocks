dc7803eb3eeda9b83761273bc3effa022a94d622
Si-Ki <vineetkr.shr@gmail.com>
2021-11-22 17:18:01 +0530

    fixes

--- config.h
+++ config.h
@@ -2,12 +2,12 @@
 static const Block blocks[] = {
 
 /*Icon*/	      /*Command*/		  /*Update Interval*/	/*Update Signal*/
+  
   {"",	"/home/noob/.scripts/memory",	  			1,	    1},
-  /*{"",  "/home/noob/.scripts/disk",              		3600,       1},*/
   {"",  "/home/noob/.scripts/volume",      			1,          10},
   {"",  "/home/noob/.scripts/nettraf",     			1,          16}, 
   {"",  "/home/noob/.scripts/clock",       			1,          1},
-  {"",	"/bin/ttf-clock",					60,	    60},
+  /*{"",  "/home/noob/.scripts/internet",              		3,          1},*/
 
 };
 
