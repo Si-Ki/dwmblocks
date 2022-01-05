00a564606a40976c01f6b320ec89f55dca21c251
Si-Ki <vineetkr.shr@gmail.com>
2022-01-05 18:39:59 +0530

    jan 5

--- config.h
+++ config.h
@@ -1,16 +1,23 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-
-/*Icon*/	      /*Command*/		  /*Update Interval*/	/*Update Signal*/
-  
-  {"",	"/home/noob/.scripts/memory",	  			1,	    1},
-  {"",  "/home/noob/.scripts/volume",      			1,          10},
-  {"",  "/home/noob/.scripts/nettraf",     			1,          16}, 
-  {"",  "/home/noob/.scripts/clock",       			1,          1},
-  /*{"",  "/home/noob/.scripts/internet",              		3,          1},*/
-
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",	"sb-music",	0,	11},
+	{"",	"sb-pacpackages",	0,	8},
+	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
+	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
+	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	{"",	"sb-torrent",	20,	7},
+	{"",	"sb-cpu",		10,	18},
+	{"",	"sb-mailbox",	60,	12},
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-nettraf",	1,	16},
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-clock",	1,	1},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " | ";
